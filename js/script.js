f12cb7bd504a611dbb359d7805dff58d425753c0
awinters81 <alexandria.winters81@gmail.com>
2022-09-03 14:04:58 -0400

    add var when opt is checked and comments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
 // Assignment code here
 
 
-// Get references to the #generate element
+// Get references to the #generate element (original comment)
 
 var generateBtn = document.querySelector("#generate");
+
+// I should have 5 options when creating the passwords,
+//And to get all possible choices, I have to create arrays for the character types.
+
+// Length of Password (I wouldn't need an array for this variable, as it's determinating the length.)
+// I think I have to create an if statement as somepoint to set parameters...
+var passLength = document.getElementById("length").value;
+
+// Character Type: Lowercase
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+// Character Type: Uppercase
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+// Character Type: Numeric
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+
+// Character Type: Special Characters
 var special = ["!","@","#","$","%","&","*"];
 
-// Write password to the #password input
+
+// In order to set the parameters for the length, I'm going to have to use a if statement, I think
+
+if (passLength < 8 || passLength > 25)
+
+// Write password to the #password input (original comment)
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -18,24 +38,14 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// .checked should allow the script to run should the checkbox connected with the corresponding
+// id be marked, once I create the proper statements for each event
+
+var checkLower = document.getElementById('lower').checked;
+var checkUpper = document.getElementById('upper').checked;
+var checkNumber = document.getElementById('number').checked;
+var checkSpecial = document.getElementById('special').checked;
+
+// Add event listener to generate button (original comment)
 generateBtn.addEventListener("click", writePassword);
 
-// I should have 5 options when creating the passwords,
-//Meaning each choice should be a if statement
-//And to get all possible choices, I have to create arrays for the character types.
-
-
-// Length of Password
-
-
-// Character Type: Lowercase
-
-
-// Character Type: Uppercase
-
-
-// Character Type: Numeric
-
-
-// Character Type: Special Characters
