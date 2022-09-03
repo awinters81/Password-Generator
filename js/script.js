f9d0b3aa43e080f754e0d136a75e4ef1793ec76f
awinters81 <alexandria.winters81@gmail.com>
2022-09-03 15:39:17 -0400

    add/test if statements and add internal comments

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ var generateBtn = document.querySelector("#generate");
 //And to get all possible choices, I have to create arrays for the character types.
 
 // Length of Password (I wouldn't need an array for this variable, as it's determinating the length.)
+// so I have to pull from my HTML?
 // I think I have to create an if statement as somepoint to set parameters...
 var passLength = document.getElementById("length").value;
 
@@ -24,19 +25,25 @@ var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 // Character Type: Special Characters
 var special = ["!","@","#","$","%","&","*"];
 
-
-// In order to set the parameters for the length, I'm going to have to use a if statement, I think
-
-if (passLength < 8 || passLength > 25)
-
 // Write password to the #password input (original comment)
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+// In order to set the parameters for the length, I'm going to have to use a if statement, I think
+// It seems I also have to create a function for the error...
+
+    function error(){
+            window.alert("Character Requierments not met!");
+        }
+
+    if (passLength < 8 || passLength > 128){
+        error();
+    }  
 
 // .checked should allow the script to run should the checkbox connected with the corresponding
 // id be marked, once I create the proper statements for each event
@@ -46,6 +53,23 @@ var checkUpper = document.getElementById('upper').checked;
 var checkNumber = document.getElementById('number').checked;
 var checkSpecial = document.getElementById('special').checked;
 
+// .concat should allow me to pull from all arrays I created earlier
+
+if(checkLower){
+    array = array.concat(lowerCase);
+}
+
+if(checkUpper){
+    array = array.concat(upperCase);
+}
+
+if (checkNumber){
+    array = array.concat(numbers);
+}
+
+if (checkSpecial){
+    array = array.concat(special);
+}
 // Add event listener to generate button (original comment)
 generateBtn.addEventListener("click", writePassword);
 
