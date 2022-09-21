9101ec05a36b9125fa24a7334528df93f2cd29be
awinters81 <alexandria.winters81@gmail.com>
2022-09-20 20:07:56 -0400

    third attempt, going about it a different way...

--- js/script.js
+++ js/script.js
@@ -9,17 +9,8 @@
 var generateBtn = document.querySelector("#generate");
 var generatePassword = document.querySelector("#placeholder")
 
-// So I need to be able to select 5 different choices in order to create my password.
-// length, lowercase, uppercase, numbers, and special characters.
-// Being that each option has multiple options to cycle through, each would need an array.
-// Except for the length.
-// So let me start by creating variables for each option.
-
-var passLength = document.getElementById("length").value;
-var checkLower = document.getElementById("lower").checked;
-var checkUpper = document.getElementById('upper').checked;
-var checkNumber = document.getElementById('number').checked;
-var checkSpecial = document.getElementById('special').checked;
+//Removing the variables for the checkboxes as I realized that I have to return
+//prompts for each option.
 
 //and let me create arrays for the characters like before...
 
@@ -27,12 +18,45 @@ var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var special = ["!","@","#","$","%","&","*"];
+var i = 0
 
 //So when I type in a number from 8-128, I should get a password with that number of characters.
-//so I think I need an if statement
+//so I think I need an if statement at some point... But let me start with the prompt...
 
-//And when I select one of the options, it should include the characters in the array, 
-//if it's not selected then it won't be included.
+function passwordOpt(){
+
+// I don't a var for length yet, so...
+// parse in Java lets me break down info that is in one form into a form that is easier to 
+// work with.
+
+  var length = parseFloat(
+    prompt('How many characters would you like?')
+  );
+
+//so, the password has to be between 8-128 characters...
+
+if(length < 8 ){
+  return null;
+}
+
+if(length > 128){
+  return null;
+}
+}
+
+//generatePassword shows up as undefined in the console. So it looks like I'd have to
+//create a password for this too as the variable password has to pull from it.
+
+function generatePassword() {
+// So to generate the password, I need my options, which I'm creating in my passwordOpt function.
+//so, it would make sense for it to be part of this function. as a variable?
+
+var pullOptions = passwordOpt();
+
+//But now this variable is not being used... I don't have all my options yet, so let me
+//go back to the 
+
+}
 
 
 
@@ -40,6 +64,7 @@ var special = ["!","@","#","$","%","&","*"];
 // Now the console states that generatePassword is not a function.
 // I think i need to add a function below that when I go to generate my password, it will 
 //generate a password based on the length specified and the options chosen.
+//So it needs to return something
 
 function writePassword() {
   var password = generatePassword();
@@ -49,5 +74,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button (original)
+//Add event listener to generate button (original)
 generateBtn.addEventListener("click", writePassword);
