9bf7a0c7cb25b945a68437455f27a616d31f48ca
awinters81 <alexandria.winters81@gmail.com>
2022-09-22 19:01:03 -0400

    adding variables for option questions

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 // Restarting again...
 
 // Get references to the #generate element (original)
-// When hitting the button, the console states that generatePassword is not defined.
+
+
 //So let me create a variable for it. I chose the placeholder because the finished password has to
 //take the place of "Your Secure Password".
 
@@ -17,8 +18,8 @@ var generatePassword = document.querySelector("#placeholder")
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
-var special = ["!","@","#","$","%","&","*"];
-var i = 0
+var specials = ["!","@","#","$","%","&","*"];
+
 
 //So when I type in a number from 8-128, I should get a password with that number of characters.
 //so I think I need an if statement at some point... But let me start with the prompt...
@@ -36,29 +37,62 @@ function passwordOpt(){
 //so, the password has to be between 8-128 characters...
 
 if(length < 8 ){
-  return null;
+ return null;
 }
 
 if(length > 128){
   return null;
-}
+} 
+
+//I tried creating these as actual functions at first, but that didn't work beacause I realized that I 
+//would have to create more variables and connect them to more functions, which doesn't make sense when I just need 
+//it to ask questions to say yes or no to a choice
+// and I realized confirm is a type of function, really, it's more like a specific type of alert just i don't have to state that
+//"window" before it. it will show up automatically.
+
+// ask if it can include lowercase
+
+var lower = confirm(
+  "Would you like lowercase characters?"
+  );
+
+// ask if it can include uppercase
+
+var upper = confirm(
+  "Would you like uppercase characters?"
+);
+
+// ask if it can include numbers
+
+var number = confirm(
+  "Would you like numbers?"
+);
+
+// ask if it can include specials
+
+var special = confirm(
+  "Would you like special characters"
+);
+
+//Okay, so now I have all these variables set up (declared) for my questions. 
+//I need to use them somehow (or have the value read...)
+
 }
 
 //generatePassword shows up as undefined in the console. So it looks like I'd have to
-//create a password for this too as the variable password has to pull from it.
+//create a function for this too as the variable password in the function writePassword has to pull from it.
 
-function generatePassword() {
+//function generatePassword() {
+
 // So to generate the password, I need my options, which I'm creating in my passwordOpt function.
 //so, it would make sense for it to be part of this function. as a variable?
 
-var pullOptions = passwordOpt();
+//var pullOptions = passwordOpt();
 
 //But now this variable is not being used... I don't have all my options yet, so let me
-//go back to the 
+//go back to the password options.
 
-}
-
-
+//}
 
 // Write password to the #password input (original)
 // Now the console states that generatePassword is not a function.
@@ -66,13 +100,14 @@ var pullOptions = passwordOpt();
 //generate a password based on the length specified and the options chosen.
 //So it needs to return something
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+ 
+// var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+  //passwordText.value = password;
 
-  passwordText.value = password;
-
-}
+//}
 
 //Add event listener to generate button (original)
-generateBtn.addEventListener("click", writePassword);
+
+//generateBtn.addEventListener("click", writePassword);
